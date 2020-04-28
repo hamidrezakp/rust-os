f6cd923286f4f6cfc92c39a596d544da843728bd
Hamid R. K. Pishghadam <hamidrkp@riseup.net>
2020-04-28 15:25:36 +0430

    Initial

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World {}", "!");
+    panic!("Some panic meassage");
+
+    loop {}
+}
