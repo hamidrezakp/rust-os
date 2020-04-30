033c0198da44aa95e287a39e3ec9b25fe541e8dc
bpmmd <bahrampour@pm.me>
2020-04-30 18:49:31 +0430

    Runing A General init Function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-#![feature(abi_x86_interrupt)]
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 use x86_64::instructions::port::Port;
@@ -54,3 +54,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         port.write(exit_code as u32);
     }
 }
+
+pub fn init() {
+    interrupts::init_idt();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,16 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World {}", "!");
+    println!("Hello World{}", "!");
+
+    blog_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
