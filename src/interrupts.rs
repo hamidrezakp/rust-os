5054ba2de719912948ff4b8fb4b68d943a67140c
bpmmd <bahrampour@pm.me>
2020-04-30 18:57:42 +0430

    A Test For Breakpoint Exception Added

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,17 +1,31 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
+use crate::{serial_print, serial_println};
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.load();
-    }
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: &mut InterruptStackFrame)
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    serial_print!("test_breakpoint_exception...");
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+    serial_println!("[ok]");
 }
