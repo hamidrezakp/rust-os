451516e98cd4936a9541ef10a81ad29b6495ed5d
bpmmd <bahrampour@pm.me>
2020-05-02 12:03:15 +0430

    Implement A Kernel Stack Overflow Fault

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,12 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
