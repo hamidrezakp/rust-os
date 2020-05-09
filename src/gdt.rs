a893a893ecaf3bbf6505497c7b36896a27a8599a
bpmmd <bahrampour@pm.me>
2020-05-09 12:45:57 +0430

    Reload the CodeSegment register and load the TaskStateSegment

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 use x86_64::VirtAddr;
 use x86_64::structures::tss::TaskStateSegment;
-use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
@@ -21,14 +21,26 @@ lazy_static! {
 }
 
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        gdt.add_entry(Descriptor::tss_segment(&TSS));
-        gdt
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (gdt, Selectors { code_selector, tss_selector })
     };
 }
 
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
+}
+
 pub fn init() {
-    GDT.load();
+    use x86_64::instructions::segmentation::set_cs;
+    use x86_64::instructions::tables::load_tss;
+
+    GDT.0.load();
+    unsafe {
+        set_cs(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector);
+    }
 }
