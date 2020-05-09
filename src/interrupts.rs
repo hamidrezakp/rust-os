52d01914d07b002cc7f5d85c2c8f356034b02333
bpmmd <bahrampour@pm.me>
2020-05-09 12:48:44 +0430

    Set the stack index for Double Fault handler in the IDT

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,4 @@
-use crate::println;
+use crate::{println, gdt};
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
@@ -6,10 +6,13 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
+        }
         idt
     };
-}
+}}
 
 pub fn init_idt() {
     IDT.load();
