d39dbd5dbd82c5ac4551f1a59ef1a9fc20838d1a
bpmmd <bahrampour@pm.me>
2020-05-15 13:42:32 +0430

    Fixing the test_println_output race condition

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -171,11 +171,14 @@ fn test_println_output() {
     serial_print!("test_println_output... ");
 
     let s = "Some test string that fits on a single line";
-    println!("{}", s);
-    for (i, c) in s.chars().enumerate() {
-        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
-        assert_eq!(char::from(screen_char.ascii_character), c);
-    }
+    interrupts::without_interrupts(|| {
+        let mut writer = WRITER.lock();
+        writeln!(writer, "\n{}", s).expect("writeln failed");
+        for (i, c) in s.chars().enumerate() {
+            let screen_char = writer.buffer.chars[BUFFER_HEIGHT - 2][i].read();
+            assert_eq!(char::from(screen_char.ascii_character), c);
+        }
+    });
 
     serial_println!("[ok]");
 }
